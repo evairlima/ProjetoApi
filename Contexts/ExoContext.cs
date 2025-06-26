977a3c7292cbd17b9475943190e7092d665243e0
evairlima <evaiir.lima@gmail.com>
2025-06-25 21:52:56 -0300

    Primeiro commit

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,19 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLSERVER;Database=ExoApi;Trusted_Connection=True;");
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
